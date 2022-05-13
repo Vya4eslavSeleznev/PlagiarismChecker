a5818128288ed7ed600af7f132343aa898538aa7
Vya4eslav111 <vya4eslav.seleznev@gmail.com>
2022-05-14 01:05:09 +0300

    Validation in /register

--- backend/static/js/register.js
+++ backend/static/js/register.js
@@ -1,27 +1,43 @@
 (() => {
     function onSignUp(event) {
         event.preventDefault();
-        console.log('111');
 
-        fetch('/register', {
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            method: "POST",
-            body: JSON.stringify({
-                name: 'JSName',
-                login: 'TEEEST',
-                password: 'JSPassword'
+        var name = document.getElementById("name").value;
+        var login = document.getElementById("login").value;
+        var password = document.getElementById("pwd").value;
+
+        if (isNotEmpty(name) && isNotEmpty(login) && isNotEmpty(password)) {
+
+            fetch('/register', {
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                method: "POST",
+                body: JSON.stringify({
+                    name: name,
+                    login: login,
+                    password: password
+                })
             })
-        }).then(res => {
-            return res.json()
-        })
-        .then(data => console.log(data))
-        .catch(error => console.log('Error'))
+        }
     }
 
     const button = document.querySelector('.signUpBtn');
-
     button.addEventListener('click', onSignUp);
 })();
 
+function isNotEmpty(field) {
+
+    var fieldData = field.value;
+
+    if (fieldData == "" || fieldData == fieldData) {
+
+        field.className = "FieldError";
+        alert("Please correct the errors in order to continue.");
+        return false;
+    } else {
+
+        field.className = "FieldOk";
+        return true; //Submits form
+    }
+}
